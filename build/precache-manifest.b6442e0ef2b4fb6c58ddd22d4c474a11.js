e1a4b0582b53afe3d3ae21c5babeb734e0b9d4d9
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2022-11-09 21:12:37 +0100

    hashrouter

---
+++ build/precache-manifest.b6442e0ef2b4fb6c58ddd22d4c474a11.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d196598157277832ec57",
+    "url": "/static/css/main.e3347bfc.chunk.css"
+  },
+  {
+    "revision": "d196598157277832ec57",
+    "url": "/static/js/main.d1965981.chunk.js"
+  },
+  {
+    "revision": "9b0ad75a30795be9f976",
+    "url": "/static/css/1.094dcf12.chunk.css"
+  },
+  {
+    "revision": "9b0ad75a30795be9f976",
+    "url": "/static/js/1.9b0ad75a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b348f9cd5b7b7ee9dffe2d9c53b80d93",
+    "url": "/index.html"
+  }
+];
